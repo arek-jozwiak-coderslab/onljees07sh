82398db5e78ae3307ecab04ee498a6a82c8214ed
arek jozwiak <arkadiusz.jozwiak@coderslab.pl>
2022-04-06 16:34:27 +0200

    day 3

--- src/main/java/pl/coderslab/book/BookRepository.java
+++ src/main/java/pl/coderslab/book/BookRepository.java
@@ -2,6 +2,7 @@ package pl.coderslab.book;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByCategoryId(Long id);
 
 
+    @Query("select b from Book b where b.title = ?1")
+    List<Book> findByTitleQuery(String title);
+
+    @Query("select b from Book b where b.category = ?1")
+    List<Book> findByCategoryQuery(Category category);
 }
